dd5fbf28a64044bf9c2e6b475064ad23e6b33975
Hamza <37643427+essolami@users.noreply.github.com>
2024-06-26 17:04:31 +0100

    implement typescript

--- utils/APIFeatures.ts
+++ utils/APIFeatures.ts
@@ -1,53 +1,61 @@
-class APIFeatures {
-  constructor(query, queryString) {
+class APIFeatures<T> {
+  private query: T;
+  private queryString: Record<string, any>;
+
+  constructor(query: T, queryString: Record<string, any>) {
     this.query = query;
     this.queryString = queryString;
   }
 
-  filter() {
+  filter(): this {
     const queryObj = { ...this.queryString };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 1B) Advanced filtering
+    // Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query = this.query.find(JSON.parse(queryStr));
+    this.query = (this.query as any).find(JSON.parse(queryStr));
 
     return this;
   }
 
-  sort() {
+  sort(): this {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
+      this.query = (this.query as any).sort(sortBy);
     } else {
-      this.query = this.query.sort("-createdAt");
+      this.query = (this.query as any).sort("-createdAt");
     }
 
     return this;
   }
 
-  limitFields() {
+  limitFields(): this {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(",").join(" ");
-      this.query = this.query.select(fields);
+      this.query = (this.query as any).select(fields);
     } else {
-      this.query = this.query.select("-__v");
+      this.query = (this.query as any).select("-__v");
     }
 
     return this;
   }
 
-  paginate() {
+  paginate(): this {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
-    this.query = this.query.skip(skip).limit(limit);
+    this.query = (this.query as any).skip(skip).limit(limit);
 
     return this;
   }
+
+  getQuery(): T {
+    return this.query;
+  }
 }
-module.exports = APIFeatures;
+
+export default APIFeatures;
